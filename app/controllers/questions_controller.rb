03db7b46f4cb4c7640fc795f1b24c8b759ad2a82
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-02-24 15:07:24 +0100

    Exercice 1 Stupid Coaching done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,18 @@
+# frozen_string_literal: true
+
+# This controller handles user questions and provides answers
+
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
     @message = params[:question]
-    if @message == "I am going to work"
-      @answer = "Great!"
-    elsif @message.include?("?")
-      @answer = "Silly question, get dressed and go to work!"
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @message == 'I am going to work'
+                'Great!'
+              elsif @message.include?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
